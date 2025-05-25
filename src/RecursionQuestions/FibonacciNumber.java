c6aa4013f2652cb5814aa6a09968a04e00e6890a
raghab007 <pokhrelraghab3@gmail.com>
2025-05-25 10:51:10 +0545

    Find fibbonacci number using recursion

---
+++ src/RecursionQuestions/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package RecursionQuestions;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(getFibonacciNumber(1));
+    }
+
+    // Finding fibonacci number using recursion...
+    public static int getFibonacciNumber(int n) {
+       if (n<=2){
+           return n;
+       }
+
+        return getFibonacciNumber(n-1)+getFibonacciNumber(n-2);
+    }
+}

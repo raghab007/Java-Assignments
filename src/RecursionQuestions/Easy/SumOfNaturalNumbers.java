9d2b6c28554e5b061ee5902e45ab7cec386a6242
raghab007 <pokhrelraghab3@gmail.com>
2025-06-03 19:34:24 +0545

    Sum of n natural numbers

--- src/RecursionQuestions/Easy/SumOfNaturalNumbers.java
+++ src/RecursionQuestions/Easy/SumOfNaturalNumbers.java
@@ -1,4 +1,15 @@
 package RecursionQuestions.Easy;
 
 public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        System.out.println(sum(10));
+    }
+    static int sum(int n){
+        if (n==0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+
 }
